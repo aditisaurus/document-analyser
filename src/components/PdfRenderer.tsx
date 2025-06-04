f721a03b7308c1f940b22cf60ae5dd359cee8781
Aditi Singh <aditisinghdeveloper@gmail.com>
2025-06-04 22:12:41 +0530

    feat: implemented upload func

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,3 +1,9 @@
-const PdfRenderer = () => {};
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  return <div></div>;
+};
 
 export default PdfRenderer;
