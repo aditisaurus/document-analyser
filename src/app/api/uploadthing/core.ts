c24c55c5fb0b22f6325e25154b3a46a0dbeba542
Aditi Singh <aditisinghdeveloper@gmail.com>
2025-06-10 22:16:10 +0530

    feat: added post upload api implementation

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,9 +5,9 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
-// import { getPineconeClient } from "@/lib/pinecone";
-// import { getUserSubscriptionPlan } from "@/lib/stripe";
-// import { PLANS } from "@/config/stripe";
+import { getPineconeClient } from "@/lib/pinecone";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
 
 const f = createUploadthing();
 
@@ -19,102 +19,102 @@ const middleware = async () => {
 
   // const subscriptionPlan = await getUserSubscriptionPlan();
 
-  return {  userId: user.id };
+  return { userId: user.id };
 };
 
-// const onUploadComplete = async ({
-//   metadata,
-//   file,
-// }: {
-//   metadata: Awaited<ReturnType<typeof middleware>>;
-//   file: {
-//     key: string;
-//     name: string;
-//     url: string;
-//   };
-// }) => {
-//   const isFileExist = await db.file.findFirst({
-//     where: {
-//       key: file.key,
-//     },
-//   });
+const onUploadComplete = async ({
+  metadata,
+  file,
+}: {
+  metadata: Awaited<ReturnType<typeof middleware>>;
+  file: {
+    key: string;
+    name: string;
+    url: string;
+  };
+}) => {
+  const isFileExist = await db.file.findFirst({
+    where: {
+      key: file.key,
+    },
+  });
 
-//   if (isFileExist) return;
+  if (isFileExist) return;
 
-//   // const createdFile = await db.file.create({
-//   //   data: {
-//   //     key: file.key,
-//   //     name: file.name,
-//   //     userId: metadata.userId,
-//   //     url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-//   //     uploadStatus: "PROCESSING",
-//   //   },
-//   // });
+  // const createdFile = await db.file.create({
+  //   data: {
+  //     key: file.key,
+  //     name: file.name,
+  //     userId: metadata.userId,
+  //     url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+  //     uploadStatus: "PROCESSING",
+  //   },
+  // });
 
-//   try {
-//     const response = await fetch(
-//       `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-//     );
+  try {
+    const response = await fetch(
+      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+    );
 
-//     const blob = await response.blob();
+    const blob = await response.blob();
 
-//     const loader = new PDFLoader(blob);
+    const loader = new PDFLoader(blob);
 
-//     const pageLevelDocs = await loader.load();
+    const pageLevelDocs = await loader.load();
 
-//     const pagesAmt = pageLevelDocs.length;
+    const pagesAmt = pageLevelDocs.length;
 
-//     // const { subscriptionPlan } = metadata;
-//     // const { isSubscribed } = subscriptionPlan;
+    // const { subscriptionPlan } = metadata;
+    // const { isSubscribed } = subscriptionPlan;
 
-//     // const isProExceeded =
-//     //   pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
-//     // const isFreeExceeded =
-//     //   pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
+    // const isProExceeded =
+    //   pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
+    // const isFreeExceeded =
+    //   pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
 
-//     // if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
-//     //   await db.file.update({
-//     //     data: {
-//     //       uploadStatus: "FAILED",
-//     //     },
-//     //     where: {
-//     //       id: createdFile.id,
-//     //     },
-//     //   });
-//     // }
+    // if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+    //   await db.file.update({
+    //     data: {
+    //       uploadStatus: "FAILED",
+    //     },
+    //     where: {
+    //       id: createdFile.id,
+    //     },
+    //   });
+    // }
 
-//     // vectorize and index entire document
-//     const pinecone = await getPineconeClient();
-//     const pineconeIndex = pinecone.Index("quill");
+    // vectorize and index entire document
+    const pinecone = await getPineconeClient();
+    const pineconeIndex = pinecone.Index("quill");
 
-//     const embeddings = new OpenAIEmbeddings({
-//       openAIApiKey: process.env.OPENAI_API_KEY,
-//     });
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
 
-//     await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
-//       pineconeIndex,
-//       namespace: createdFile.id,
-//     });
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+      namespace: createdFile.id,
+    });
 
-//     await db.file.update({
-//       data: {
-//         uploadStatus: "SUCCESS",
-//       },
-//       where: {
-//         id: createdFile.id,
-//       },
-//     });
-//   } catch (err) {
-//     await db.file.update({
-//       data: {
-//         uploadStatus: "FAILED",
-//       },
-//       where: {
-//         id: createdFile.id,
-//       },
-//     });
-//   }
-// };
+    await db.file.update({
+      data: {
+        uploadStatus: "SUCCESS",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  } catch (err) {
+    await db.file.update({
+      data: {
+        uploadStatus: "FAILED",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  }
+};
 
 export const ourFileRouter = {
   freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
